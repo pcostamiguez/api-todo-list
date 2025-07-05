43954e1fe195cfc56476c9c47ef4111f27286523
Pedro Henrique Costa Miguez <pcostamiguez@gmail.com>
2025-07-05 16:33:00 -0300

    Todo Service

---
+++ src/main/java/br/dev/phcm/api_todo_list/services/TodoService.java
@@ -0,0 +1,39 @@
+package br.dev.phcm.api_todo_list.services;
+
+import br.dev.phcm.api_todo_list.entities.Todo;
+import br.dev.phcm.api_todo_list.repositories.TodoRepositoryInterface;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    private TodoRepositoryInterface todoRepository;
+
+    public TodoService(TodoRepositoryInterface todoRepository){
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> list(){
+        Sort sort = Sort.by("prioridade").descending()
+                .and(Sort.by("nome").ascending());
+
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
