10e57570d3714cef027c7a5da202f2b84be145f9
Pedro Henrique Costa Miguez <pcostamiguez@gmail.com>
2025-07-05 15:31:50 -0300

    first commit

---
+++ src/main/java/br/dev/phcm/api_todo_list/ApiTodoListApplication.java
@@ -0,0 +1,13 @@
+package br.dev.phcm.api_todo_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/phcm/api_todo_list/ApiTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.phcm.api_todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
