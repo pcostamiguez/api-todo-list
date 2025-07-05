0f93df9110db29555668fa79eb2c468cbef52b29
Pedro Henrique Costa Miguez <pcostamiguez@gmail.com>
2025-07-05 17:55:34 -0300

    Created test

--- src/test/java/br/dev/phcm/api_todo_list/ApiTodoListApplicationTests.java
+++ src/test/java/br/dev/phcm/api_todo_list/ApiTodoListApplicationTests.java
@@ -1,13 +1,51 @@
 package br.dev.phcm.api_todo_list;
 
+import br.dev.phcm.api_todo_list.entities.Todo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiTodoListApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void testCreateTodoSuccess() {
+		var todo = new Todo(
+				"Tarefa de test 1",
+				"Descricao da tarefa 1",
+				false,
+				3
+		);
+
+		webTestClient
+				.post()
+				.uri("/todos")
+				.bodyValue(todo)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody()
+				.jsonPath("$").isArray()
+				.jsonPath("$.length()").isEqualTo(1)
+				.jsonPath("$[0].nome").isEqualTo(todo.getNome())
+				.jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
+				.jsonPath("$[0].realizado").isEqualTo(todo.getRealizado())
+				.jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
+	}
+
+	@Test
+	void testCreateTodoFailure() {
+		webTestClient
+				.post()
+				.uri("/todos")
+				.bodyValue(
+						new Todo("", "", false, 0)
+				)
+				.exchange()
+				.expectStatus().isBadRequest();
 	}
 
 }
